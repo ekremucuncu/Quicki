0b236b061a39c742e6c0cfd86798acb7cc00651e
ekremucuncu <59807398+ekremucuncu@users.noreply.github.com>
2022-02-17 17:11:51 +0300

    second

---
+++ quicki.js
@@ -0,0 +1,7 @@
+$("#phone").click(function() {
+  $('#phone-desktop').attr("src","images/iphone.png")
+});
+
+$("#desktop").click(function() {
+  $('#phone-desktop').attr("src","images/desktop.png")
+});
